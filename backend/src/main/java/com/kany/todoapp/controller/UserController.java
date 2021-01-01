0cb8e782c1cf59564314f1e679f9f7aeb42e036a
Leehojun <rainbow_83@naver.com>
2021-01-01 19:52:39 +0900

    swagger api 설명 추카

--- backend/src/main/java/com/kany/todoapp/controller/UserController.java
+++ backend/src/main/java/com/kany/todoapp/controller/UserController.java
@@ -2,9 +2,9 @@ package com.kany.todoapp.controller;
 
 import com.kany.todoapp.domain.User;
 import com.kany.todoapp.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,24 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-
     private final UserService userService;
 
+    @ApiOperation("사용자 이름으로 조회")
+    @ApiImplicitParam(name = "name", value = "사용자 닉네임")
     @GetMapping("/{name}")
     public ResponseEntity<User> find(@PathVariable String name) {
-        LOGGER.debug("call find user ({})", name);
 
         User user = userService.findByName(name);
 
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/{name}", consumes = "application/json")
-    public ResponseEntity<User> change(@RequestBody User user, @PathVariable String name) {
-        LOGGER.debug("call change user ({}), change name ({})", user.toString(), name);
+    @ApiOperation("사용자 변경")
+    @ApiImplicitParam(name = "name", value = "변경할 닉네임")
+    @PostMapping("/{name}")
+    public ResponseEntity<User> change(@PathVariable String name) {
 
-        User changeUser = userService.change(user, name);
+        User changeUser = userService.change(name);
 
         return new ResponseEntity<>(changeUser, HttpStatus.OK);
     }
--- backend/src/main/java/com/kany/todoapp/domain/Post.java
+++ backend/src/main/java/com/kany/todoapp/domain/Post.java
@@ -1,5 +1,8 @@
 package com.kany.todoapp.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -8,23 +11,30 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(hidden = true)
     private Long id;
 
     @Column(nullable = false)
     private String content;
 
+    @ApiModelProperty(hidden = true)
     private LocalDateTime created_at = LocalDateTime.now();
 
+    @ApiModelProperty(hidden = true)
     private LocalDateTime updated_at;
 
     @Column(nullable = false)
+    @ApiModelProperty(hidden = true)
     private boolean completed;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name="user_id")
+    @ApiModelProperty(hidden = true)
     private User user;
 }
