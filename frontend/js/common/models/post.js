37cbd3ba9b988e85797e1c1b1819fe227f35e511
DDD05 <dhzm2aud@naver.com>
2021-01-05 17:49:37 +0900

    [#12] add models

---
+++ frontend/js/common/models/post.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Post = void 0;
+var Post = /** @class */ (function () {
+    function Post(id, content, createdAt, completed) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.completed = completed;
+    }
+    return Post;
+}());
+exports.Post = Post;
+//# sourceMappingURL=post.js.map
