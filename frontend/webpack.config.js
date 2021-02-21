a2359122c736702735fc2a2346d7a4502cb2516a
DDD05 <dhzm2aud@naver.com>
2021-02-22 01:58:13 +0900

    webpack setting 수정
    - dotenv-webpack 추가
    - env 환경 셋팅

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,36 +1,45 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
-
-module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				include: [path.resolve(__dirname, 'src/js')],
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
+module.exports = env => {
+	console.log("[🐶 DDD] ~ file: webpack.config.js ~ line 6 ~ env.mode", env.mode)
+	return {
+		mode: 'development',
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'main.js',
+		},
+		// module: {
+		// 	rules: [
+		// 		{
+		// 			test: /\.js$/,
+		// 			include: [path.resolve(__dirname, 'src')],
+		// 			exclude: /node_modules/,
+		// 			use: {
+		// 				loader: 'babel-loader',
+		// 				options: {
+		// 					presets: ['@babel/preset-env'],
+		// 				},
+		// 			},
+		// 		},
+		// 	],
+		// },
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src')
+			}
+		},
+		plugins: [
+			new Dotenv({
+				path: `.env.${env.mode}`,
+			}),
 		],
-	},
-	plugins: [
-		new Dotenv()
-	],
-	devServer: {
-		contentBase: path.resolve(__dirname, ''),
-		compress: true,
-		disableHostCheck: true,
-		host: "0.0.0.0" // default : 127.0.0.1
-	},
+		devServer: {
+			contentBase: path.resolve(__dirname),
+			publicPath: '/',
+			host: '0.0.0.0', // default : 127.0.0.1
+			overlay: true,
+		},
+	};
 };
