6dbea1245cc89173f4754fa779d1a70ad7e131b6
DDD05 <dhzm2aud@naver.com>
2021-05-14 21:34:07 +0900

    [feature] node express server #43 - mock data test 01

--- express_server/app.js
+++ express_server/app.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+
+const fs = require('fs');
+const FILE_PATH = './data.json';
+
+const loadFile = (filePath = FILE_PATH, options) => {
+	const defaultOptions = { resType: 'JSON' };
+	const { resType } = { ...defaultOptions, ...options };
+
+	const res = fs.readFileSync(filePath, 'utf-8');
+
+	if (resType && resType === 'JSON') return JSON.parse(res);
+	return res;
+};
+
+const getWorkSpace = ({ id, title, user }) => {
+	const jsonData = loadFile();
+  console.log("[🐱 DDD] ~ file: app.js ~ line 21 ~ getWorkSpace ~ jsonData", jsonData)
+	const { workspaces } = jsonData;
+
+	return workspaces.find(ws => {
+		return (id && ws.id === id) || (title && ws.title === title) || (user && ws.users.some(({ uid }) => uid === user));
+	});
+};
 
 app.listen(PORT, () => {
-  console.log(
-    `\n%cApp express server listen : ${PORT}\n`,
-    'margin-bottom:4px;color:#1ab394;font-size:14px;font-weight:bold;line-height:1.2em;border-bottom:2px solid #1ab394',
-  )
-})
+	console.log(`🐱 App express server listen : ${PORT}`);
+});
+
+app.get('/workspace', (req, res) => {
+	const { id, title, user } = req.query;
+  console.log("[🐱 DDD] ~ file: app.js ~ line 33 ~ app.get ~ id, title, user ", id, title, user )
+	const data = getWorkSpace({ id, title, user });
+	res.send(data);
+});
